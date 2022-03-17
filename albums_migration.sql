fa6cce607b6b9fbd7c561a59cf386a8b5e0a7d56
Noah Bruno <noah.bruno122@gmail.com>
2022-03-17 16:10:54 -0500

    slect

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,11 +2,12 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
-CREATE TABLE albums(
+CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist_name VARCHAR(50) NOT NULL,
     record_name VARCHAR(50) NOT NULL,
-    release_date DATE NOT NULL,
-    sales FLOAT UNSIGNED NOT NULL,
+    release_date INT NOT NULL,
+    sales FLOAT NOT NULL,
     genre VARCHAR(50) NOT NULL,
-)
+PRIMARY KEY(id)
+);
