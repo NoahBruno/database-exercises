8a000c1f5a6a0e6e6eeb87d0851b41479db5150f
Noah Bruno <noah.bruno122@gmail.com>
2022-03-19 16:41:33 -0500

    data

---
+++ where_exercises.sql
@@ -0,0 +1,10 @@
+USE employees;
+
+SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT last_name FROM employees WHERE last_name LIKE 'E%';
+SELECT last_name FROM employees WHERE last_name LIKE '%q%';
+SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya') OR first_name = 'Maya';
+SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
+SELECT last_name FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+SELECT last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
