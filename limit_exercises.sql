ed63b911121c4457c300303df7320a9a7f683388
Noah Bruno <noah.bruno122@gmail.com>
2022-03-21 10:07:59 -0500

    data

---
+++ limit_exercises.sql
@@ -0,0 +1,12 @@
+
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
+
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 4 OFFSET 46;
+
+SELECT emp_no FROM employees ORDER BY emp_no;
+
+SELECT emp_no FROM employees ORDER BY emp_no LIMIT 5 OFFSET 45;
